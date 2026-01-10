369f85a4cb7a567134e7e2335bc43f56fc8266a3
sulthan1030 <muhammadsulthan200@gmail.com>
2026-01-10 20:17:37 +0800

    Fix indentation and formatting in API routes for better readability

--- routes/api.php
+++ routes/api.php
@@ -1,34 +1,37 @@
-<?php
+    <?php
 
-use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\AuthController;
-use App\Http\Controllers\SongController;
-use App\Http\Controllers\AlbumController;
-use App\Http\Controllers\ArtistController;
-use App\Http\Controllers\PlaylistController;
+    use Illuminate\Http\Request;
+    use Illuminate\Support\Facades\Route;
+    use App\Http\Controllers\AuthController;
+    use App\Http\Controllers\SongController;
+    use App\Http\Controllers\AlbumController;
+    use App\Http\Controllers\ArtistController;
+    use App\Http\Controllers\PlaylistController;
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "api" middleware group. Make something great!
-|
-*/
+    /*
+    |--------------------------------------------------------------------------
+    | API Routes
+    |--------------------------------------------------------------------------
+    |
+    | Here is where you can register API routes for your application. These
+    | routes are loaded by the RouteServiceProvider and all of them will
+    | be assigned to the "api" middleware group. Make something great!
+    |
+    */
 
-// Auth Routes
-Route::post('register', [AuthController::class, 'register']);
-Route::post('login', [AuthController::class, 'login']);
-Route::middleware('auth:api')->group(function () {
-    Route::post('logout', [AuthController::class, 'logout']);
-    Route::post('refresh', [AuthController::class, 'refresh']);
-    Route::get('me', [AuthController::class, 'me']);
-    
-    Route::apiResource('songs', SongController::class);
-    Route::apiResource('albums', AlbumController::class);
-    Route::apiResource('artists', ArtistController::class);
-    Route::apiResource('playlists', PlaylistController::class);
-});
+    // Auth Routes
+    Route::post('register', [AuthController::class, 'register']);
+    Route::post('login', [AuthController::class, 'login']);
+    Route::middleware('auth:api')->group(function () {
+        Route::post('logout', [AuthController::class, 'logout']);
+        Route::post('refresh', [AuthController::class, 'refresh']);
+        Route::get('me', [AuthController::class, 'me']);
+        
+        Route::apiResource('songs', SongController::class);
+        Route::apiResource('albums', AlbumController::class);
+        Route::apiResource('artists', ArtistController::class);
+        Route::apiResource('playlists', PlaylistController::class);
+
+        Route::post('playlists/{id}/songs', [PlaylistController::class, 'addSong']);
+        Route::delete('playlists/{id}/songs/{songId}', [PlaylistController::class, 'removeSong']);
+    });
